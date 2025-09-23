11129fe6921c098f63fde040084f76de7e9da358
Cezar Braga <seu-email@dominio.com>
2025-09-22 21:31:22 -0300

    fix(tfss): autofix pages (pass 1)

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,2 +1,10 @@
-
-
+export default function Page() {
+  return (
+    <main className="p-10 text-center">
+      <h1 className="text-3xl font-bold mb-4">Página Não Encontrada</h1>
+      <p className="text-neutral-700 dark:text-neutral-300">
+        A página que você procura não existe ou foi movida.
+      </p>
+    </main>
+  );
+}
